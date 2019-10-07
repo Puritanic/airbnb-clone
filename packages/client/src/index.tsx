bfef189d826dc76f66308aeecfc260755717d98d
Darkø Tasevski <darko.tasevski@redbox.com>
2019-10-07 23:12:09 +0200

    feature: add register form validation schema

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './routes';
-import { ApolloProvider } from 'react-apollo';
+import React from "react";
+import ReactDOM from "react-dom";
+import Routes from "./routes";
+import { ApolloProvider } from "react-apollo";
 
-import './index.css';
+import "./index.css";
 
-import * as serviceWorker from './serviceWorker';
-import apolloClient from './apollo';
+import * as serviceWorker from "./serviceWorker";
+import apolloClient from "./apollo";
 
 ReactDOM.render(
   <ApolloProvider client={apolloClient}>
     <Routes />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- packages/client/src/modules/Register/view/RegisterView.tsx
+++ packages/client/src/modules/Register/view/RegisterView.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Form, Icon, Input, Button } from "antd";
 import { FormikErrors, withFormik, FormikProps } from "formik";
+import * as yup from "yup";
 
 interface FormValues {
   email: string;
@@ -15,14 +16,24 @@ export class RegisterView extends Component<
   FormikProps<FormValues> & RegisterViewProps
 > {
   render() {
-    const { values, handleBlur, handleChange, handleSubmit } = this.props;
+    const {
+      values,
+      handleBlur,
+      handleChange,
+      handleSubmit,
+      touched,
+      errors
+    } = this.props;
 
     return (
       <form
         onSubmit={handleSubmit}
         style={{ width: "300px", margin: "15rem auto", textAlign: "center" }}
       >
-        <Form.Item>
+        <Form.Item
+          help={touched.email && errors.email ? errors.email : ""}
+          validateStatus={touched.email && errors.email ? "error" : undefined}
+        >
           <Input
             name="email"
             type="email"
@@ -33,7 +44,12 @@ export class RegisterView extends Component<
             onBlur={handleBlur}
           />
         </Form.Item>
-        <Form.Item>
+        <Form.Item
+          help={touched.password && errors.password ? errors.password : ""}
+          validateStatus={
+            touched.password && errors.password ? "error" : undefined
+          }
+        >
           <Input
             name="password"
             prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
@@ -70,7 +86,26 @@ export class RegisterView extends Component<
   }
 }
 
+const emailNotLongEnough = "email must be at least 3 characters";
+const passwordNotLongEnough = "password must be at least 3 characters";
+const invalidEmail = "email must be a valid email";
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .min(3, emailNotLongEnough)
+    .max(255)
+    .email(invalidEmail)
+    .required(),
+  password: yup
+    .string()
+    .min(3, passwordNotLongEnough)
+    .max(255)
+    .required()
+});
+
 export default withFormik<RegisterViewProps, FormValues>({
+  validationSchema,
   mapPropsToValues: () => ({ email: "", password: "" }),
   handleSubmit: async (values, { props, setErrors, setSubmitting }) => {
     const errors = await props.submit(values);
