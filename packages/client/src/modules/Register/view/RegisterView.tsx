a2821aed9f6498bb9e67363755eb5664f7092546
Darkø Tasevski <darko.tasevski@redbox.com>
2019-10-08 00:07:26 +0200

    refactor: move shared yup validation logic to common workspace

--- packages/client/src/modules/Register/view/RegisterView.tsx
+++ packages/client/src/modules/Register/view/RegisterView.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Form, Icon, Input, Button } from "antd";
 import { FormikErrors, withFormik, FormikProps } from "formik";
-import * as yup from "yup";
+import { userValidationSchema } from "@abb/common";
 
 interface FormValues {
   email: string;
@@ -86,26 +86,8 @@ export class RegisterView extends Component<
   }
 }
 
-const emailNotLongEnough = "email must be at least 3 characters";
-const passwordNotLongEnough = "password must be at least 3 characters";
-const invalidEmail = "email must be a valid email";
-
-const validationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .min(3, emailNotLongEnough)
-    .max(255)
-    .email(invalidEmail)
-    .required(),
-  password: yup
-    .string()
-    .min(3, passwordNotLongEnough)
-    .max(255)
-    .required()
-});
-
 export default withFormik<RegisterViewProps, FormValues>({
-  validationSchema,
+  validationSchema: userValidationSchema,
   mapPropsToValues: () => ({ email: "", password: "" }),
   handleSubmit: async (values, { props, setErrors, setSubmitting }) => {
     const errors = await props.submit(values);
